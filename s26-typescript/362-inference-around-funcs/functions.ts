5e50acbb7d548e38b7abb3bebdb84369da806e02
Vanessa T <tai.v.tsang@gmail.com>
2023-03-28 12:53:25 +0100

    Completed lecture 362

---
+++ s26-typescript/362-inference-around-funcs/functions.ts
@@ -0,0 +1,30 @@
+/* Annotations Around Functions:
+** The value to are going to assign to the variable declaration on the left. */
+/*
+// Before annotation on an arrow func:
+const add = (a, b) => {
+  return a + b;
+};
+
+// After annotation the same func:
+// Annotation both the params and the actual func itself.
+const add = (a: number, b: number): number => {
+  return a + b;
+};
+*/
+
+/* Inference Around Functions:
+1. Annotation all params.
+2. Annotate what the func will return.
+
+*/
+
+/* If we return a string when we had stated we'll return a number, then we'll get a 
+red underline advising us that we have an error or bug. */
+
+const add = (a: number, b: number): number => {
+  return "Some plain text here";
+};
+
+//  TS will not make sure we have the correct logic code inside of the func.
+// Therefore, for the output part of func we add in type annotation in case we miss-out the return keyword from the return statement.
