a418668e54df173555ec51daaed92f7ace17a3e7
Vanessa T <tai.v.tsang@gmail.com>
2023-05-10 17:11:52 +0100

    Completed lecture 14

---
+++ s2-types-around-props-and-states/14-type-inference-with-state/14-type-inference-with-state.md
@@ -0,0 +1,25 @@
+# 14. Type Inference with State
+
+When user click on the btn, we need to take the current name and add it to an Array piece of state. An array of strings that stores the entire list of guests - all different names we collected over time. This is why we now need to write a new piece of state and initialise with an empty array since we will need to store our guests whom we wish to invite to our party in here.
+
+## button
+Add on onClick prop and inside it define an inline func to invoke but to make life easier we'll define it just above the return method as here we can write all the logic we need to add a new guest.
+
+## input element
+Take whatever the user has typed inside of here and add that as a new String into our guests array. 
+***onClick function***
+Then within the onClick func, clear out the current value of the input/empty it entirly. So for this we use the setName piece of state or the setter func and pass-in empty string. 
+Then take the current value of name, add it to our guests array, by calling setGuests func. Pass-in arry of name as this will delete any previous guests we've added into our app.
+
+Take the current array of guests, take all the different strings out of it, add it to an new array, and then add it to a new array, and then add on name to the very end. Now we have error of `never[]` which is array of never. For the guests arry we initialise with empty array but since its empty TS have no idea what types of value should be stored in here. This is why the guests piece of state is type of never[] as TS has no idea what type of values is going to go inside of this array. So annotate this useState hook: 
+
+```js 
+// Our guests piece of state will be an array of Strings.
+const [guests, setGuests] = useState<string[]>([]);
+```
+
+## Test in UI
+`npm start` tested and found that the input and button displays as expected. Now, users can type inside of the input box and click the submit btn.
+
+### The conclusion
+It is fine to initialise a piece of state with empty string as Type Inference will know that the `name` is of type. But if we put in default value where Type Inference don't known then we need to provide a type for that piece of state so annote that state.
---
+++ s2-types-around-props-and-states/14-type-inference-with-state/src/state/GuestList.tsx
@@ -0,0 +1,48 @@
+/************************************************************************* 
+Party guest list web app:
+Write the component.
+Export it at the bottom.
+Show this component inside of index.tsx file.
+****************************************************************************/
+
+/* 
+To track whatever user types into input we need to add in a piece of state so we the useState hook
+
+Annotate component with React.FC but without any angle brackets 
+sice we don't expect to receive any props. Return a simple div 
+element to display a little simple plain text.
+*/
+
+// To update a piece of state, import useState hook
+import { useState } from 'react';
+
+const GuestList: React.FC = () => {
+  // Add in a piece of state using useState hook and initialise it with empty string.
+  const [name, setName] = useState('');
+  // Two pieces of state destructured from the useState hook. Initialise with empty array as here we'll store our guests whom we'll invite to our party.
+  const [guests, setGuests] = useState<string[]>([]);
+
+  // All the logic we need to add a new guest.
+  // take the current value of name, add it to our guests array, by calling setGuests func. setGuests() func, pass-in array of names as this will delete
+  // any previous guests we've added into our app.
+  // Take the current array of guests, take all the different strings out of it, add it to an new array, and then add it to a new array, and then add on name to the very end.
+  const onClick = () => {
+    setName('');
+    setGuests([...guests, name]);
+  };
+
+  //  Add an input element and a button element. onChange callback
+  //  and a value prop. To track this we add in a piece of state - useState hook.
+  //  Whenever our onChange callback func is called we'll update our setName piece of state.
+  // input element within this JSX, take whatever user has typed inside of here, add that as a new String into our guests array.
+  return (
+    <div>
+      <h3>Vanessa's Big Birthday Guest List</h3>
+      <input value={name} onChange={(e) => setName(e.target.value)} />
+      <button onClick={onClick}>Add Guest</button>
+    </div>
+  );
+};
+
+// Export this component so we can use it inside of components for files.
+export default GuestList;
